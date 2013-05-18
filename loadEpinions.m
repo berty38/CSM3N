595329ff8e70686b723d6d144b01acd4c2786cff
Ben London <blondon@cs.umd.edu>
2013-05-18 19:56:36 -0400

    edited epinions loader

--- loadEpinions.m
+++ loadEpinions.m
@@ -1,10 +1,10 @@
 %% Load data
+epinions = load('epinions/epinions-small.txt');
+n = max(max(epinions(:,1:2)));
+gt = sparse(epinions(:,1),epinions(:,2),2*epinions(:,3)-1,n,n);
 % load 'epinions/epinions-big.txt'
 % epinions_big(:,1:2) = epinions_big(:,1:2) + 1;
 % gt = spconvert(epinions_big);
-load 'epinions/epinions-small.txt'
-epinions_small(:,3) = 2*epinions_small(:,3) - 1;
-gt = spconvert(epinions_small);
 
 %% Snowball sample a reasonably sized connected component
 
