be23c95098df6d27cecf3b86c7e3e4a4b57e212e
Ben London <blondon@cs.umd.edu>
2013-05-21 08:52:13 -0400

    fixed disagreements in loadEpinions

--- loadEpinions.m
+++ loadEpinions.m
@@ -1,16 +1,24 @@
 %% Load data
+
 epinions = load('epinions/epinions-small.txt');
 n = max(max(epinions(:,1:2)));
 gt = sparse(epinions(:,1),epinions(:,2),2*epinions(:,3)-1,n,n);
-inconsistent = nnz(abs(triu(gt-gt')) == 2)
-unrecip_pos = nnz(triu(gt==1 & gt'==0)) + nnz(triu(gt==0 & gt'==1))
-unrecip_neg = nnz(triu(gt==-1 & gt'==0)) + nnz(triu(gt==0 & gt'==-1))
-recip_pos = nnz(triu(gt==gt' & gt==1))
-recip_neg = nnz(triu(gt==gt' & gt==-1))
 
 % load 'epinions/epinions-big.txt'
 % epinions_big(:,1:2) = epinions_big(:,1:2) + 1;
 % gt = spconvert(epinions_big);
 
-%% Snowball sample a reasonably sized connected component
+%% Fix disagreements
+
+% Make disagreements negative
+gt(abs(triu(gt-gt'))==2) = -1;
+% Make unreciprocated positives positive
+gt(triu(gt==1 & gt'==0)) = 1;
+gt(triu(gt==0 & gt'==1)) = 1;
+% Make unreciprocated negatives negative
+gt(triu(gt==-1 & gt'==0)) = -1;
+gt(triu(gt==0 & gt'==-1)) = -1;
+% Upper-triangulize
+gt = triu(gt);
+
 
